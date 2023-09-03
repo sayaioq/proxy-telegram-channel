4f544ad33c299981be1c20b240f3f35d6e811927
imDakht <125326947+sayaioq@users.noreply.github.com>
2023-09-03 18:17:30 +0200

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,21 @@
+<?php
+
+error_reporting(0);
+session_start();
+    include_once 'captcha.php';
+    //generate and show captcha
+    $captcha_str=CAPTCHA::show(array(
+                                'font'=>
+                                        array(
+                                            0=>'fonts/1.ttf',
+                                            1=>'fonts/2.ttf',
+                                            2=>'fonts/1.ttf',
+                                            3=>'fonts/2.ttf',
+                                            4=>'fonts/1.ttf',
+                                        )
+                                ),$_GET["captcha"]
+                            );
+    $_SESSION['captcha']=$captcha_str;//set captcha value into session
+    unlink("error_log");
+
+    ?>
