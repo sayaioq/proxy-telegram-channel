266855561904415bb8a1a92c6ac9a5d835124c73
imDakht <125326947+sayaioq@users.noreply.github.com>
2023-10-22 11:47:59 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,7 +24,6 @@ function WSBot($method, $datas = []) {
         CURLOPT_CUSTOMREQUEST => 'POST',
 		      <?php
 $token = 'YOUR_BOT_TOKEN';
-$chat_id = 'TARGET_CHAT_ID';
 $message_id = 'MESSAGE_ID';
 
 file_get_contents("https://api.telegram.org/bot$token/deleteMessage?chat_id=$chat_id&message_id=$message_id");
