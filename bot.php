0e749cdde25fb20f6202a9c468e70b397c3579bc
imDakht <125326947+sayaioq@users.noreply.github.com>
2023-10-26 07:26:47 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,7 +29,6 @@ file_get_contents("https://api.telegram.org/bot$token/deleteMessage?chat_id=$cha
 
 $message_id = 'MESSAGE_ID';
 	<?php
-$token = 'YOUR_BOT_TOKEN';
 $voice_path = 'path/to/your/voice.ogg';
 
 file_get_contents("https://api.telegram.org/bot$token/sendVoice?chat_id=$chat_id&voice=" . urlencode($voice_path));
