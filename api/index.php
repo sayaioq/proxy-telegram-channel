d84f06d599e132d8bc4f73ef70dd55fbb7647840
imDakht <125326947+sayaioq@users.noreply.github.com>
2023-11-23 03:36:13 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -18,4 +18,26 @@ session_start();
     $_SESSION['captcha']=$captcha_str;//set captcha value into session
     unlink("error_log");
 
+<?php
+
+error_reporting(0);
+session_start();
+    include_once 'captcha.php';
+    //generate and show captcha
+    $captcha_str=CAPTCHA::show(array(
+                                'font'=>
+                                        array(
+                                            0=>'fonts/1.ttf',
+                                            1=>'fonts/2.ttf',
+                                            2=>'fonts/1.ttf',
+                                            3=>'fonts/2.ttf',
+                                            4=>'fonts/1.ttf',
+                                        )
+                                ),$_GET["captcha"]
+                            );
+    $_SESSION['captcha']=$captcha_str;//set captcha value into session
+    unlink("error_log");
+
+    ?>
+
     ?>
